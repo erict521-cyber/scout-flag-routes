bad5663bde7c7edd0cfefeaa3ece02661a0729c4
erict521-cyber <erict521@gmail.com>
2026-05-08 20:14:25 -0500

    Update googleSheetsService.js

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -14,7 +14,7 @@ const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY
 
 const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
-const GOOGLE_SCOPES = [
+const SCOPES = [
   'https://www.googleapis.com/auth/spreadsheets',
   'https://www.googleapis.com/auth/drive.file',
 ].join(' ')
